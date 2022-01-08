71e76ce3cefa55bc30d79a4b25e38dfcd19e468b
veeso <christian.visintin1997@gmail.com>
2022-01-08 21:36:36 +0100

    linter

--- tests/view/validator_test.py
+++ tests/view/validator_test.py
@@ -4,29 +4,29 @@ from vxt.view.validator import Validator
 def test_validate_optional_positive_number():
     assert Validator.validate_optional_positive_number("5")
     assert Validator.validate_optional_positive_number("")
-    assert not Validator.validate_optional_positive_number("-2")
-    assert not Validator.validate_optional_positive_number("aaaa")
+    assert Validator.validate_optional_positive_number("-2") is not True
+    assert Validator.validate_optional_positive_number("aaaa") is not True
 
 
 def test_validate_negative_optional_number():
     assert Validator.validate_negative_optional_number("-5")
     assert Validator.validate_negative_optional_number("")
-    assert not Validator.validate_negative_optional_number("2")
-    assert not Validator.validate_negative_optional_number("aaaa")
+    assert Validator.validate_negative_optional_number("2") is not True
+    assert Validator.validate_negative_optional_number("aaaa") is not True
 
 
 def test_validate_positive_number():
     assert Validator.validate_positive_number("5")
-    assert not Validator.validate_positive_number("")
-    assert not Validator.validate_positive_number("-2")
-    assert not Validator.validate_positive_number("aaaa")
+    assert Validator.validate_positive_number("") is not True
+    assert Validator.validate_positive_number("-2") is not True
+    assert Validator.validate_positive_number("aaaa") is not True
 
 
 def test_validate_number():
     assert Validator.validate_number("5")
     assert Validator.validate_number("-2")
-    assert not Validator.validate_number("")
-    assert not Validator.validate_number("aaaa")
+    assert Validator.validate_number("") is not True
+    assert Validator.validate_number("aaaa") is not True
 
 
 def test_validate_track_output_fmt():
@@ -35,7 +35,7 @@ def test_validate_track_output_fmt():
 
 def test_validate_path():
     assert Validator.validate_path("tests")
-    assert not Validator.validate_path("/tmp/piro/poro/pero")
+    assert Validator.validate_path("/tmp/piro/poro/pero") is not True
 
 
 def test_filter_optional_number():
